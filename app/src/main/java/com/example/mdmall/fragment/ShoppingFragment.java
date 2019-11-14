b3ba7956b45ed0558f6703c749c9930f63ae6973
wtq <17611127821@163.com>
2019-11-14 17:10:05 +0800

    11.14 购物车UI 更多推荐（无选择联动） 实名认证逻辑 搜索框流式布局 推荐列表wtq

--- app/src/main/java/com/example/mdmall/fragment/ShoppingFragment.java
+++ app/src/main/java/com/example/mdmall/fragment/ShoppingFragment.java
@@ -7,15 +7,24 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mdmall.BaseFragment;
 import com.example.mdmall.R;
+import com.example.mdmall.adapter.ShopingCarAdapter;
+import com.example.mdmall.adapter.WeiNiTuiJianAdapter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ShoppingFragment extends BaseFragment {
     private static ShoppingFragment shoppingFragment;
     private View view;
+    private ShopingCarAdapter shopingCarAdapter;
+    private WeiNiTuiJianAdapter weiNiTuiJianAdapter;
+
     public static ShoppingFragment getInstance() {
         if (shoppingFragment == null) shoppingFragment = new ShoppingFragment();
         return shoppingFragment;
@@ -23,12 +32,36 @@ public class ShoppingFragment extends BaseFragment {
 
     public ShoppingFragment() {
     }
+
+    @BindView(R.id.recy)
+    RecyclerView recy;
+    @BindView(R.id.recy_weinituijian)
+    RecyclerView recy_weinituijian;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (view == null) view = inflater.inflate(R.layout.fragment_shopping, container, false);
         ButterKnife.bind(this, view);
        // EventBus.getDefault().register(this);
+        initShopingCarAdapter();
+        initWeiNiTuiJianAdapter();
         return view;
+    }
+
+    private void initWeiNiTuiJianAdapter() {
+        recy_weinituijian.setNestedScrollingEnabled(false);
+        if(weiNiTuiJianAdapter==null){
+            weiNiTuiJianAdapter = new WeiNiTuiJianAdapter(getActivity());
+            recy_weinituijian.setLayoutManager(new GridLayoutManager(getActivity(),2));
+            recy_weinituijian.setAdapter(weiNiTuiJianAdapter);
+        }
+    }
+
+    private void initShopingCarAdapter() {
+        if(shopingCarAdapter==null){
+            shopingCarAdapter = new ShopingCarAdapter(getActivity());
+            recy.setAdapter(shopingCarAdapter);
+            recy.setLayoutManager(new LinearLayoutManager(getActivity()));
+        }
     }
 }
